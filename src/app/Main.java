181716b9af4f58205b444d1f5ecb1dcd5c3673de
Mark-lu-051030 <61438635+Mark-lu-051030@users.noreply.github.com>
2024-06-15 21:28:21 -0400

    Update Main.java
    merge two Main class

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,15 @@
 package src.app;
 
+import javax.swing.*;
+import java.awt.*;
+import UI.ViewManager;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(":)");
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new ViewManager();  // Create and show the GUI managed by ViewMasnager.
+            }
+        });
     }
 }
