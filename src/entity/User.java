3b90ca5451ce6c9c6b7ce71a667f776c0ddc93c9
Mark <3188560574lmx051030@gmail.com>
2024-07-18 20:24:17 -0400

    Created User and Conversation Entities

--- src/entity/User.java
+++ src/entity/User.java
@@ -1,4 +1,51 @@
-package src.entity;
+package entity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class User {
+    private String userName;
+    private String password;
+    private String email;
+    private List<Conversation> conversations;
+
+    public User(String userName, String password, String email) {
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+        this.conversations = new ArrayList<>();
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return password;
+    }
+
+    public void setUserPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserEmail() {
+        return email;
+    }
+
+    public void setUserEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Conversation> getConversations() {
+        return conversations;
+    }
+
+    public void addConversation(Conversation conversation) {
+        this.conversations.add(conversation);
+    }
+
 }
