e2f42b449d3a5973de8c5113c4af1f19b0671883
yangs169 <shuyao.yang@mail.utoronto.ca>
2024-07-19 10:37:39 -0400

    shawn is building LoginService now.

--- src/use_case/LoginService.java
+++ src/use_case/LoginService.java
@@ -1,11 +1,23 @@
 package use_case;
+import java.io.File;
 
 public class LoginService
 {
 
     private final String TEMP_USER_DATA = "..\\localdata\\userdata.txt";
+    private String username;
+    private String password;
 
-    private LoginService(String userName, String password)
+    private LoginService(String input_userName, String input_password)
+    {
+        this.username = input_userName;
+        this.password = input_password;
+
+        checkFile();
+        //check if userdata.txt exists, if not create it.
+    }
+
+    private void checkFile()
     {
 
     }
