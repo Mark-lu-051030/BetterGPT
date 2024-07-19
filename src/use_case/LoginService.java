a7e9564f2e407c69ea8b88f80eef98a144c7dc97
yangs169 <shuyao.yang@mail.utoronto.ca>
2024-07-19 10:32:33 -0400

    shawn add something in use_case -> Loginservice

--- src/use_case/LoginService.java
+++ src/use_case/LoginService.java
@@ -1,7 +1,12 @@
 package use_case;
 
-public class LoginService {
-    private LoginService(String userName, String password) {
+public class LoginService
+{
+
+    private final String TEMP_USER_DATA = "..\\localdata\\userdata.txt";
+
+    private LoginService(String userName, String password)
+    {
 
     }
 }
