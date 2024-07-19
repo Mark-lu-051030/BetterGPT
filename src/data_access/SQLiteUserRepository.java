4831cd89da16b6cec109e8078fd584c7959eb3ab
Mark Lu <3188560574@qq.com>
2024-07-19 10:36:40 -0400

    Updated database

--- src/data_access/SQLiteUserRepository.java
+++ src/data_access/SQLiteUserRepository.java
@@ -4,5 +4,17 @@ import entity.*;
 import java.sql.*;
 
 public class SQLiteUserRepository {
-    private static final String URL =
+    private static final String URL = "jdbc:sqlite:users.db";
+
+    public SQLiteUserRepository() {
+        createTable();
+    }
+
+    private void createTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS users ("
+                + "username TEXT PRIMARY KEY,"
+                + "password TEXT NOT NULL,"
+                + "email TEXT NOT NULL,)";
+        //HANDLER: TRY CATCH
+    }
 }
