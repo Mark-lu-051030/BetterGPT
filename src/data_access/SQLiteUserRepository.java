7c6e6d863909a6e659138ba6855a621e4907d47c
Mark Lu <3188560574@qq.com>
2024-07-19 11:06:43 -0400

    Updated database

--- src/data_access/SQLiteUserRepository.java
+++ src/data_access/SQLiteUserRepository.java
@@ -14,7 +14,28 @@ public class SQLiteUserRepository {
         String sql = "CREATE TABLE IF NOT EXISTS users ("
                 + "username TEXT PRIMARY KEY,"
                 + "password TEXT NOT NULL,"
-                + "email TEXT NOT NULL,)";
-        //HANDLER: TRY CATCH
+                + "email TEXT NOT NULL)";
+
+        try (Connection conn = DriverManager.getConnection(URL);
+             Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    public void addUser(User user) throws SQLException {
+        String sql = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+        try (Connection conn = DriverManager.getConnection(URL);
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, user.getUserName());
+            pstmt.setString(2, user.getUserPassword());
+            pstmt.setString(3, user.getUserEmail());
+            pstmt.executeUpdate();
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
