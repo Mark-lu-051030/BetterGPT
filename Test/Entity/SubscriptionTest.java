849c4590b8ce63e98811a32b082825aa795d57f8
Zain T <zain.tariq@mail.utoronto.ca>
2024-07-21 21:09:26 -0400

    basic tests for subscription

---
+++ Test/Entity/SubscriptionTest.java
@@ -0,0 +1,37 @@
+package Entity;
+
+import junit.framework.TestCase;
+import entity.Subscription;
+
+
+
+public class SubscriptionTest extends TestCase {
+
+    private Subscription subscription;
+
+
+    public void setUp() {
+        subscription = new Subscription("Basic", 3600); // 1 hour in seconds
+    }
+
+
+    public void testGetLevel() {
+        assertEquals("Basic", subscription.getLevel());
+    }
+
+
+    public void testGetTime() {
+        assertEquals(3600, subscription.getTime());
+    }
+
+
+    public void testSetLevel() {
+        subscription.setLevel("Premium");
+        assertEquals("Premium", subscription.getLevel());
+    }
+
+    public void testSetTime() {
+        subscription.setTime(7200); // 2 hours in seconds
+        assertEquals(7200, subscription.getTime());
+    }
+}
