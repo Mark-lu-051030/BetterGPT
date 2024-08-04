4d787e0275e4098f6ced69a3258a20079638fadf
yangs169 <shuyao.yang@mail.utoronto.ca>
2024-08-04 16:48:54 -0400

    shawn is writing superclass of tier in Entity folder

--- src/entity/Tier.java
+++ src/entity/Tier.java
@@ -1,10 +1,13 @@
 package entity;
 
-public class Tier
+public abstract class Tier
 {
     /*
     This is a super class for subscription. In this class we set up some basic and public properties
     of all tiers.
      */
-    private 
+    private double unitfee; // Unit fee of this tier. for example $1 per hour.
+    private boolean isexpired; // Check if this tier is expired;
+    private boolean isopen; //Check if the user starts to use the time.
+
 }
