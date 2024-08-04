d2bdee1794b099a10dba1880b25c95b0ece2f583
yangs169 <shuyao.yang@mail.utoronto.ca>
2024-08-04 18:03:28 -0400

    shawn is doing Monthlytier

--- src/entity/Tier/MonthTier.java
+++ src/entity/Tier/MonthTier.java
@@ -1,7 +1,31 @@
 package entity.Tier;
 
 import entity.Tier.Tier;
+import java.util.Date;
+import java.util.Timer;
 
 public class MonthTier extends Tier
 {
+    /*
+
+        This is a monthly bill method.
+     */
+    private Date start_date;
+    private int number_of_months;
+
+    public MonthTier(double inputunitfee,Date inputstart, int inputmonth)
+    {
+        this.unitfee = inputunitfee;
+        this.start_date = inputstart;
+        this.number_of_months = inputmonth;
+        this.isopen = false;
+
+    }
+
+    private void checkExpired()
+    {
+
+    }
+
+
 }
