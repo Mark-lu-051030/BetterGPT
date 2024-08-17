f8478d82f0e30a7bd2937efcee801afb4a1855da
Zain T <zain.tariq@mail.utoronto.ca>
2024-08-16 21:46:50 -0400

    for sending email

---
+++ src/use_case/ActuallySendEmail.java
@@ -0,0 +1,55 @@
+package use_case;
+
+
+import javax.mail.*;
+
+import javax.mail.internet.*;
+import java.util.Properties;
+
+public class ActuallySendEmail {
+
+    private static final String FROM = "bettergptpassreset@gmail.com";
+    private static final String USERNAME = "bettergptpassreset@gmail.com";
+    private static final String APP_PASSWORD = "qplk emye huuo szbo"; // your generated App Password
+    private static final String HOST = "smtp.gmail.com";
+
+    public static void sendEmail(String to, String subject, String body) {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", HOST);
+        props.put("mail.smtp.port", "587");
+
+        // Get the Session object
+        Session session = Session.getInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(USERNAME, APP_PASSWORD);
+            }
+        });
+
+        try {
+            // Create a default MimeMessage object
+            Message message = new MimeMessage(session);
+
+            // Set From: header field of the header
+            message.setFrom(new InternetAddress(FROM));
+
+            // Set To: header field of the header
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+
+            // Set Subject: header field
+            message.setSubject(subject);
+
+            // Now set the actual message
+            message.setText(body);
+
+            // Send message
+            Transport.send(message);
+
+            System.out.println("Sent message successfully....");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
