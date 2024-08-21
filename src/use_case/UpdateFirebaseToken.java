e894bee0c4001f2094f75ccd99ee2515d520952f
Zain T <zain.tariq@mail.utoronto.ca>
2024-08-20 23:13:24 -0400

    added changes to firebase token

--- src/use_case/UpdateFirebaseToken.java
+++ src/use_case/UpdateFirebaseToken.java
@@ -4,6 +4,10 @@ import data_access.FirebaseTokenRepository;
 import data_access.EmailTokenRepository;
 import app.FirebaseInitializer;
 
+import data_access.UserRepository;
+
+import entity.User;
+
 
 import data_access.FirebaseUserRepository;
 
@@ -16,6 +20,16 @@ public class UpdateFirebaseToken {
         this.token = token;
 
     }
+    /*
+    public String getUsername(){
+        FirebaseInitializer.initialize();
+        EmailTokenRepository tokenRepository = new FirebaseTokenRepository();
+
+        String corespondingUsername = tokenRepository.
+
+    }
+
+     */
 
     public void performUpdate() {
         FirebaseInitializer.initialize();
@@ -53,34 +67,85 @@ public class UpdateFirebaseToken {
             userRepository.updatePassword(username, newPassword);
         });
     }
-}
-/* for testing delete later
-    public static void main(String[] args) {
-        UpdateFirebaseToken updateFirebaseToken = new UpdateFirebaseToken("12@gmail.com", "testtoken");
-        updateFirebaseToken.changePassword("ZA12", "!!!");
+
+    public String getUsername() {
+        FirebaseInitializer.initialize();
+        FirebaseTokenRepository tokenRepository = new FirebaseTokenRepository();
+        String getUsername = String.valueOf(tokenRepository.FindUserByEmail(email));
+        return getUsername;
+    }
+
+    public void findUsernameByEmailandChangePSWD(String email, UserRepository userRepository, String changePSWD) {
+        userRepository.findByEmail(email, new UserRepository.UserCallback() {
+            @Override
+            public void onCallback(User user) {
+                if (user != null) {
+                    String username = user.getUserName();
+                    userRepository.updatePassword(username, changePSWD);
+                    System.out.println("Username found: " + username);
+                } else {
+                    System.out.println("User not found with email: " + email);
+                }
+            }
+        });
     }
 }
 
-    /*
+
+/*
     public static void main(String[] args) {
+        String testEmail = "testuser@example.com";
+        String testToken = "aw1";
+
+        // Instantiate the UpdateFirebaseToken class with test data
+        UpdateFirebaseToken updateFirebaseToken = new UpdateFirebaseToken(testEmail, testToken);
+
+        FirebaseInitializer.initialize();
+
+
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            FirebaseUserRepository userRepository = new FirebaseUserRepository();
+
+            // Call the performUpdate() method to test it
+            updateFirebaseToken.findUsernameByEmailandChangePSWD(testEmail, userRepository, "newpass");
+
+
+        });
+    }
+}
+
+ */
+
+
+
         /*
+        //UpdateFirebaseToken updateFirebaseToken = new UpdateFirebaseToken("12@gmail.com", "testtoken");
+        //updateFirebaseToken.changePassword("ZA12", "!!!");
+    }
+}
+
+
+    public static void main(String[] args) {
+
         UpdateFirebaseToken updateTest1 = new UpdateFirebaseToken("user1122223@example.com", "new_token151");
         updateTest1.deleteUser();
 
-         */
+
+
+
         // Create a new user object
-        /*
+
         userRepository.updatePassword("123", "qoqo");
         System.out.println("Password changed successfully");
 
-        */
-        //FirebaseInitializer.initialize();
-        //javax.swing.SwingUtilities.invokeLater(() -> {
-            //FirebaseUserRepository userRepository = new FirebaseUserRepository();
-            //userRepository.updatePassword("test1", "fiu");
-
+         */
 
         /*
+        FirebaseInitializer.initialize();
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            FirebaseUserRepository userRepository = new FirebaseUserRepository();
+            //userRepository.updatePassword("test1", "fiu");
+
         User newUser = new User("ZA12", "password11212", "12@gmail.com");
         userRepository.updateUser(newUser);
 
@@ -88,13 +153,8 @@ public class UpdateFirebaseToken {
 
         System.out.println("User testsubject1 added to Firebase.");
 
-         */
-
-
-
-
-
 
         //});
     //}
-//}
+
+         */
